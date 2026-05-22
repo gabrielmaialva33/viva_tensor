d1f7c6ca7c5c3bc1b99f0a3a054827ffcbf0fcf8
Gabriel Maia <gabrielmaialva33@gmail.com>
2026-05-21 23:45:38 -0300

    feat: implement Marlin W4A16 weight prepacking NIF and add integration planning documentation

--- zig_src/nif_marlin_w4a16.c
+++ zig_src/nif_marlin_w4a16.c
@@ -7,6 +7,9 @@
  */
 
 #include "viva_nif.h"
+extern "C" {
+#include "viva_marlin_pack.h"
+}
 
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
@@ -16,6 +19,42 @@
 extern int marlin_cuda(const void *A, const void *B, void *C, void *s, int prob_m, int prob_n,
                        int prob_k, void *workspace, int groupsize, int dev, cudaStream_t stream,
                        int thread_k, int thread_n, int sms, int max_par);
+
+typedef struct {
+    int K, N, groupsize, groups;
+    void *d_B;
+    void *d_s;
+    void *d_workspace;
+    size_t b_bytes;
+    size_t s_bytes;
+    size_t ws_bytes;
+} MarlinPackedResource;
+
+extern "C" ErlNifResourceType *marlin_packed_resource_type = NULL;
+
+extern "C" void marlin_packed_resource_dtor(ErlNifEnv *env, void *obj) {
+    (void)env;
+    MarlinPackedResource *r = (MarlinPackedResource *)obj;
+    if (!r)
+        return;
+    if (r->d_B) {
+        cudaFree(r->d_B);
+        r->d_B = NULL;
+    }
+    if (r->d_s) {
+        cudaFree(r->d_s);
+        r->d_s = NULL;
+    }
+    if (r->d_workspace) {
+        cudaFree(r->d_workspace);
+        r->d_workspace = NULL;
+    }
+}
+
+static ERL_NIF_TERM marlin_error_code(ErlNifEnv *env, const char *reason, int code) {
+    return enif_make_tuple3(env, enif_make_atom(env, "error"), enif_make_atom(env, reason),
+                            enif_make_int(env, code));
+}
 
 static void fill_random_fp16(uint16_t *dst, size_t n) {
     uint32_t x = 0x12345678u;
@@ -178,4 +217,139 @@ extern "C" ERL_NIF_TERM viva_marlin_w4a16_bench_nif(ErlNifEnv *env, int argc,
         return enif_make_atom(env, errbuf);
     }
     return enif_make_int(env, 0);
+}
+
+extern "C" ERL_NIF_TERM viva_marlin_w4a16_prepack_nif(ErlNifEnv *env, int argc,
+                                                      const ERL_NIF_TERM argv[]) {
+    if (argc != 5)
+        return marlin_error_code(env, "bad_arity", -1);
+
+    ErlNifBinary w_bin;
+    ErlNifBinary s_bin;
+    int K = 0, N = 0, groupsize = 0;
+    if (!enif_inspect_binary(env, argv[0], &w_bin))
+        return marlin_error_code(env, "invalid_weight_binary", -2);
+    if (!enif_inspect_binary(env, argv[1], &s_bin))
+        return marlin_error_code(env, "invalid_scale_binary", -3);
+    if (!enif_get_int(env, argv[2], &K) || !enif_get_int(env, argv[3], &N) ||
+        !enif_get_int(env, argv[4], &groupsize))
+        return marlin_error_code(env, "invalid_dimensions", -4);
+    if (K <= 0 || N <= 0)
+        return marlin_error_code(env, "invalid_dimensions", -5);
+    if (groupsize != -1 && (groupsize <= 0 || K % groupsize != 0))
+        return marlin_error_code(env, "invalid_groupsize", -6);
+    if (K % 16 != 0 || N % 128 != 0)
+        return marlin_error_code(env, "invalid_marlin_shape", -7);
+
+    int groups = (groupsize == -1) ? 1 : (K / groupsize);
+    size_t w_bytes = (size_t)K * (size_t)N * sizeof(uint16_t);
+    size_t s_bytes = (size_t)groups * (size_t)N * sizeof(uint16_t);
+    size_t b_bytes = ((size_t)K * (size_t)N) / 2u;
+    size_t ws_bytes = ((size_t)N / 128u) * 16u * sizeof(int);
+
+    if (w_bin.size != w_bytes)
+        return marlin_error_code(env, "weight_size_mismatch", -8);
+    if (s_bin.size != s_bytes)
+        return marlin_error_code(env, "scale_size_mismatch", -9);
+
+    uint32_t *out_B = (uint32_t *)malloc(b_bytes);
+    uint16_t *out_s = (uint16_t *)malloc(s_bytes);
+    if (!out_B || !out_s) {
+        free(out_B);
+        free(out_s);
+        return marlin_error_code(env, "host_alloc_failed", -10);
+    }
+
+    int pack_rc = viva_marlin_pack((const uint16_t *)w_bin.data, (const uint16_t *)s_bin.data, K, N,
+                                   groupsize, out_B, out_s);
+    if (pack_rc != 0) {
+        free(out_B);
+        free(out_s);
+        return marlin_error_code(env, "pack_failed", pack_rc);
+    }
+
+    void *d_B = NULL;
+    void *d_s = NULL;
+    void *d_workspace = NULL;
+    MarlinPackedResource *resource = NULL;
+    ERL_NIF_TERM term;
+    cudaError_t err = cudaMalloc(&d_B, b_bytes);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+    err = cudaMalloc(&d_s, s_bytes);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+    err = cudaMalloc(&d_workspace, ws_bytes);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+    err = cudaMemcpy(d_B, out_B, b_bytes, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+    err = cudaMemcpy(d_s, out_s, s_bytes, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+    err = cudaMemset(d_workspace, 0, ws_bytes);
+    if (err != cudaSuccess)
+        goto cuda_fail;
+
+    free(out_B);
+    free(out_s);
+
+    resource = (MarlinPackedResource *)enif_alloc_resource(marlin_packed_resource_type,
+                                                           sizeof(MarlinPackedResource));
+    if (!resource) {
+        cudaFree(d_workspace);
+        cudaFree(d_s);
+        cudaFree(d_B);
+        return marlin_error_code(env, "resource_alloc_failed", -20);
+    }
+
+    resource->K = K;
+    resource->N = N;
+    resource->groupsize = groupsize;
+    resource->groups = groups;
+    resource->d_B = d_B;
+    resource->d_s = d_s;
+    resource->d_workspace = d_workspace;
+    resource->b_bytes = b_bytes;
+    resource->s_bytes = s_bytes;
+    resource->ws_bytes = ws_bytes;
+
+    term = enif_make_resource(env, resource);
+    enif_release_resource(resource);
+    return term;
+
+cuda_fail:
+    int cuda_code = (int)err;
+    if (d_workspace)
+        cudaFree(d_workspace);
+    if (d_s)
+        cudaFree(d_s);
+    if (d_B)
+        cudaFree(d_B);
+    free(out_B);
+    free(out_s);
+    return marlin_error_code(env, "cuda_failed", cuda_code);
+}
+
+extern "C" ERL_NIF_TERM viva_marlin_w4a16_get_b_bytes_nif(ErlNifEnv *env, int argc,
+                                                          const ERL_NIF_TERM argv[]) {
+    if (argc != 1)
+        return marlin_error_code(env, "bad_arity", -1);
+
+    MarlinPackedResource *resource = NULL;
+    if (!enif_get_resource(env, argv[0], marlin_packed_resource_type, (void **)&resource))
+        return marlin_error_code(env, "invalid_resource", -2);
+
+    ErlNifBinary bin;
+    if (!enif_alloc_binary(resource->b_bytes, &bin))
+        return marlin_error_code(env, "binary_alloc_failed", -3);
+
+    cudaError_t err = cudaMemcpy(bin.data, resource->d_B, resource->b_bytes, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) {
+        enif_release_binary(&bin);
+        return marlin_error_code(env, "cuda_failed", (int)err);
+    }
+
+    return enif_make_binary(env, &bin);
 }
